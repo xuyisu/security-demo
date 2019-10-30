09724bd0ad2a1bb1364b41db27355b032e9cdc96
ponymym <1246979748@qq.com>
2019-10-30 20:52:50 +0800

    验证码实现

--- security-validate-code/src/main/java/com/yisu/validate/code/controller/ValidateCodeController.java
+++ security-validate-code/src/main/java/com/yisu/validate/code/controller/ValidateCodeController.java
@@ -1,5 +1,7 @@
 package com.yisu.validate.code.controller;
 
+import org.springframework.social.connect.web.HttpSessionSessionStrategy;
+import org.springframework.social.connect.web.SessionStrategy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,7 @@ public class ValidateCodeController {
     /**
      * 操作session的工具类
      */
-//    private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
+    private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
 
     @GetMapping("/code/image")
     public void createCode(HttpServletRequest request, HttpServletResponse response)
