93391b0cfca408a0fa1cf27a47d55cfdc6f58217
ponymym <1246979748@qq.com>
2019-11-16 21:31:32 +0800

    微信登录

---
+++ security-auth-wechat/src/main/java/com/yisu/authentication/mobile/SmsCodeAuthenticationSecurityConfig.java
@@ -0,0 +1,50 @@
+/**
+ *
+ */
+package com.yisu.authentication.mobile;
+
+import com.yisu.authentication.FwAuthenctiationFailureHandler;
+import com.yisu.authentication.FwAuthenticationSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xuyisu
+ *
+ */
+@Component
+public class SmsCodeAuthenticationSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+	@Autowired
+	private FwAuthenticationSuccessHandler fwAuthenticationSuccessHandler;
+
+	@Autowired
+	private FwAuthenctiationFailureHandler fwAuthenctiationFailureHandler;
+
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		SmsCodeAuthenticationFilter smsCodeAuthenticationFilter = new SmsCodeAuthenticationFilter();
+		smsCodeAuthenticationFilter.setAuthenticationManager(http.getSharedObject(AuthenticationManager.class));
+		smsCodeAuthenticationFilter.setAuthenticationSuccessHandler(fwAuthenticationSuccessHandler);
+		smsCodeAuthenticationFilter.setAuthenticationFailureHandler(fwAuthenctiationFailureHandler);
+
+		SmsCodeAuthenticationProvider smsCodeAuthenticationProvider = new SmsCodeAuthenticationProvider();
+		smsCodeAuthenticationProvider.setUserDetailsService(userDetailsService);
+
+		http.authenticationProvider(smsCodeAuthenticationProvider)
+			.addFilterAfter(smsCodeAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+	}
+
+}
---
+++ security-auth-wechat/src/main/java/com/yisu/config/SecuritySessionBeanConfig.java
@@ -0,0 +1,30 @@
+package com.yisu.config;
+
+import com.yisu.authentication.session.FwExpiredSessionStrategy;
+import com.yisu.authentication.session.FwInvalidSessionStrategy;
+import com.yisu.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.session.InvalidSessionStrategy;
+import org.springframework.security.web.session.SessionInformationExpiredStrategy;
+
+@Configuration
+public class SecuritySessionBeanConfig {
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(InvalidSessionStrategy.class)
+    public InvalidSessionStrategy invalidSessionStrategy(){
+        return new FwInvalidSessionStrategy(securityProperties.getBrowser().getSession().getSessionInvalidUrl());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(SessionInformationExpiredStrategy.class)
+    public SessionInformationExpiredStrategy sessionInformationExpiredStrategy(){
+        return new FwExpiredSessionStrategy(securityProperties.getBrowser().getSession().getSessionInvalidUrl());
+    }
+
+}
