d64dc4a7eb590ad9b839f66c2546a77c562eef58
ponymym <1246979748@qq.com>
2019-11-17 17:50:14 +0800

    sql

---
+++ sql/security-demo.sql
@@ -0,0 +1,48 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50703
+Source Host           : localhost:3306
+Source Database       : security-demo
+
+Target Server Type    : MYSQL
+Target Server Version : 50703
+File Encoding         : 65001
+
+Date: 2019-11-17 17:49:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for fw_userconnection
+-- ----------------------------
+DROP TABLE IF EXISTS `fw_userconnection`;
+CREATE TABLE `fw_userconnection` (
+  `userId` varchar(255) NOT NULL,
+  `providerId` varchar(255) NOT NULL,
+  `providerUserId` varchar(255) NOT NULL,
+  `rank` int(11) NOT NULL,
+  `displayName` varchar(255) DEFAULT NULL,
+  `profileUrl` varchar(512) DEFAULT NULL,
+  `imageUrl` varchar(512) DEFAULT NULL,
+  `accessToken` varchar(512) NOT NULL,
+  `secret` varchar(512) DEFAULT NULL,
+  `refreshToken` varchar(512) DEFAULT NULL,
+  `expireTime` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`userId`,`providerId`,`providerUserId`),
+  UNIQUE KEY `UserConnectionRank` (`userId`,`providerId`,`rank`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for persistent_logins
+-- ----------------------------
+DROP TABLE IF EXISTS `persistent_logins`;
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
