7da5174a77429bd6e2eb20755aabe280ccb9716a
bore <www.964092342@qq.com>
2019-11-22 13:53:10 +0800

    sso 调整

---
+++ security-oauth2-sso/security-oauth2-sso-server/src/main/java/com/yisu/FwSsoServerApplication.java
@@ -0,0 +1,17 @@
+package com.yisu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuyisu
+ *
+ */
+@SpringBootApplication
+public class FwSsoServerApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FwSsoServerApplication.class, args);
+	}
+
+}
