b1941ec0f3d9fa4442a0a233407f6c57ae9dbe34
bore <www.964092342@qq.com>
2019-11-30 10:53:42 +0800

    SSO 优化

--- security-oauth2-sso/security-oauth2-sso-client1/src/main/java/com/yisu/client/FwSsoClient1Application.java
+++ security-oauth2-sso/security-oauth2-sso-client1/src/main/java/com/yisu/client/FwSsoClient1Application.java
@@ -6,9 +6,6 @@ package com.yisu.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xuyisu
@@ -17,11 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @EnableOAuth2Sso
 @SpringBootApplication
-public class SsoClient1Application {
+public class FwSsoClient1Application {
 	
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsoClient1Application.class, args);
+		SpringApplication.run(FwSsoClient1Application.class, args);
 	}
 	
 }
--- security-oauth2-sso/security-oauth2-sso-client2/src/main/java/com/yisu/client/FwSsoClient2Application.java
+++ security-oauth2-sso/security-oauth2-sso-client2/src/main/java/com/yisu/client/FwSsoClient2Application.java
@@ -6,9 +6,6 @@ package com.yisu.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xuyisu
@@ -16,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SsoClient2Application {
+public class FwSsoClient2Application {
 	
 
 	public static void main(String[] args) {
-		SpringApplication.run(SsoClient2Application.class, args);
+		SpringApplication.run(FwSsoClient2Application.class, args);
 	}
 	
 }
